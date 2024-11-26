b6e531d02bbbbbb845906390e826a7dcffba0ccb
surajkr20 <suraj71308kumar@gmail.com>
2024-11-26 16:58:56 +0530

    add conversation and message model

--- backend/models/comment.model.js
+++ backend/models/comment.model.js
@@ -18,4 +18,4 @@ const commentSchema = new mongoose.Schema({
     }
 })
 
-export const comment = mongoose.model('Comment', commentSchema)
+export const Comment = mongoose.model('Comment', commentSchema)
