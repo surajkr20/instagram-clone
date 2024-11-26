8317adb834dc398459b3ebc5d71195ca86747135
surajkr20 <suraj71308kumar@gmail.com>
2024-11-26 16:10:48 +0530

    add user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+  },
+  profilepic: {
+    type: String,
+    default: "",
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  post: [
+    {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Post"
+    }
+  ],
+  bookmark: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    }
+  ]
+},{timestamps: true});
+
+export const User = mongoose.model('User', userSchema)
